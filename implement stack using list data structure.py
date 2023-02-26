c55ed90a0387d3adca8e75035fc04256e55cb509
Gauransh Goel <121503835+Gauranshgoel123@users.noreply.github.com>
2023-02-26 20:48:39 +0530

    Add files via upload

---
+++ implement stack using list data structure.py
@@ -0,0 +1,10 @@
+stack = ["Amar", "Akbar", "Anthony"]
+stack.append("Ram")
+stack.append("Iqbal")
+print(stack)
+# Removes the last item
+print(stack.pop())
+print(stack)
+# Removes the last item
+print(stack.pop())
+print(stack)
