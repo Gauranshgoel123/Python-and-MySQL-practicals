cd72feaa2ef8ab48e99a27dfc7a62ae404c8ffb5
Gauransh Goel <121503835+Gauranshgoel123@users.noreply.github.com>
2023-02-26 20:50:21 +0530

    Add files via upload

---
+++ implement queue using list data structure.py
@@ -0,0 +1,10 @@
+q=[]
+q.append(10)
+q.append(100)
+q.append(1000)
+q.append(10000)
+print("Initial Queue is:",q)
+print(q.pop(0))
+print(q.pop(0))
+print(q.pop(0))
+print("After Removing elements:",q)
