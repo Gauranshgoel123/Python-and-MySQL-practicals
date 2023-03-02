2a53a29b5ef3e13488835d12bad323ff6e41fa70
Gauransh Goel <121503835+Gauranshgoel123@users.noreply.github.com>
2023-03-02 22:25:37 +0530

    Add files via upload

---
+++ python database connectivity script that deletes records from category table of database items that have name=’stockable’.py
@@ -0,0 +1,11 @@
+import mysql.connector
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="myusername",
+  password="mypassword",
+  database="mydatabase")
+mycursor = mydb.cursor()
+sql = "DELETE FROM customers WHERE address = 'Mountain 21'"
+mycursor.execute(sql)
+mydb.commit()
+print(mycursor.rowcount, "record(s) deleted")
